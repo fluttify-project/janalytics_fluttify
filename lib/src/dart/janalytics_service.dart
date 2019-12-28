86452eb2b9273f305e3898f0ed32fe441e6d2acf
yohom <yohombao@gmail.com>
2019-12-28 22:51:09 +0800

    enhance: 完善init方法

--- lib/src/dart/janalytics_service.dart
+++ lib/src/dart/janalytics_service.dart
@@ -3,16 +3,23 @@ import 'package:janalytics_fluttify/src/android/android.export.g.dart';
 import 'package:janalytics_fluttify/src/ios/ios.export.g.dart';
 
 class JAnalyticsService {
-  static Future<void> init({@required String iosKey}) async {
+  static Future<void> init({
+    @required String iosKey,
+    String channel = 'developer-default',
+  }) async {
     await platform(
       android: (pool) async {
-        cn_jiguang_analytics_android_api_JAnalyticsInterface
-            .init(await android_app_Application.get());
+        final context = await android_app_Application.get();
+
+        await cn_jiguang_analytics_android_api_JAnalyticsInterface
+            .init(context);
+        await cn_jiguang_analytics_android_api_JAnalyticsInterface.setChannel(
+            context, channel);
       },
       ios: (pool) async {
         final config = await JANALYTICSLaunchConfig.create__();
         await config.set_appKey(iosKey);
-        await config.set_channel('developer-default');
+        await config.set_channel(channel);
 
         await JANALYTICSService.setupWithConfig(config);
 
